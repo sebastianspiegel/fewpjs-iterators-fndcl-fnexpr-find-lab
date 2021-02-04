17eb9ac77becf58426fecf1d66dfd7ce8e4de9ba
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-02-04 12:36:44 -0500

    commit tree b8850aaf191c2214c1252cdeb2bcf5e60e0e59f6

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(array) {
+  let win = array.find( i => i.result === "W" )
+  return win ? win.year : undefined
+}
